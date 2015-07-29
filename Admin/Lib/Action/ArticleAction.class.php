c5f02d831536cd57c7d1728aee228273cc26c84e
guolei <guolei@chunbo.com>
2015-07-29 10:39:26 +0800

    修改router_id写错

--- Admin/Lib/Action/ArticleAction.class.php
+++ Admin/Lib/Action/ArticleAction.class.php
@@ -381,7 +381,7 @@ class ArticleAction extends BaseAction
 
         $model = D('Router');
         if ($type == 1) {
-            return $model->save($rouder_id, $rule, $link);
+            return $model->save($router_id, $rule, $link);
         } else if ($type == 2) {
             return $model->update($router_id, $rule, $link);
         }
